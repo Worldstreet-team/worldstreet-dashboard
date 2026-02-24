c608307107a26047a5be4af447473b76aa927195
Amirize Wisdom <97132123+chukwumela909@users.noreply.github.com>
2026-02-24 15:13:07 +0100

    fix: remove unnecessary closing tag in Layout component

--- src/app/(DashboardLayout)/layout.tsx
+++ src/app/(DashboardLayout)/layout.tsx
@@ -115,7 +115,7 @@ export default function Layout({
                               </div>
                             </div>
                           </div>
-                          )}
+                      
                           {/* Wallet PIN Setup Modal */}
                           <PinSetupModal />
                         </DashboardVividProvider>
